70bfb0f34b9b5f88fa5a5a8b2d32173a9f361e50
AntiEevee <eefjekarremans@hotmail.nl>
2021-10-17 18:50:34 +0200

    Add IntentChange on Click of Appointment

--- SSApp/app/src/main/java/nl/ekarremans/slotenservice/AppointmentItem.java
+++ SSApp/app/src/main/java/nl/ekarremans/slotenservice/AppointmentItem.java
@@ -2,7 +2,11 @@ package nl.ekarremans.slotenservice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import nl.ekarremans.slotenservice.models.Appointment;
 
 public class AppointmentItem extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class AppointmentItem extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointment_item);
+
+        final View appointment = findViewById(R.id.calender_appointment_item);
+        appointment.setOnClickListener(this::OpenAppointment);
+    }
+
+    private void OpenAppointment(View view) {
+        Intent intent = new Intent(AppointmentItem.this, Appointment.class);
+//        Todo: Set and give appointment ID
+        intent.putExtra("AppID","AppointmentID");
     }
 }
